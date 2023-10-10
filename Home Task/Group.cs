d1b6626c4aa393d4a6a1f546576c6a6c64865aa3
Vagif0728 <tu7j4ryqj@code.edu.az>
2023-10-11 02:00:31 +0400

    new tasks

---
+++ Home Task/Group.cs
@@ -0,0 +1,57 @@
+﻿//using System;
+//using System.Collections.Generic;
+//using System.Linq;
+//using System.Text;
+//using System.Threading.Tasks;
+
+//namespace Home_Task
+//{
+//    internal class Group
+//    {
+//        public string GroupName { get; set; }
+
+//        public int[] Students { get; set; }
+
+
+//        public Group(string groupName, int[] students)
+//        {
+//            GroupName = groupName;
+//            Students = students;
+//        }
+
+
+//        public void Add()
+//        {
+
+
+//            string[] students = new string[5];
+
+//            for (int i = 0; i < students.Length; i++)
+//            {
+//                students[i] = Console.ReadLine();
+//            }
+
+
+
+//        }
+
+//        public void GetAll() 
+//        {
+//            for (int i = 0; i <students.Length; i++)
+//            {
+//                Console.WriteLine(student[i]);
+//            }
+//        }
+
+//        public void GetOnlineStudents()
+//        {
+
+//        }
+
+//        public void GetOflineStudents()
+//        {
+
+//        }
+//    }
+//}
+//}
---
+++ Home Task/Program.cs
@@ -0,0 +1,53 @@
+﻿//namespace Home_Task
+//{
+//    internal class Program
+//    {
+//        static void Main(string[] args)
+//        {
+//            //Animal animal = new Animal("Jack");
+//            //Dog dog = new Dog("Jacks","Pitbull");
+
+
+
+
+//            Group group =new Group();
+
+//            group.Add();
+//        }
+//    }
+
+
+    //internal class Animal
+    //{
+
+    //    public string Gender { get; set; }
+
+    //    public int Birthday
+    //    {
+    //        get
+    //        {
+    //            return DateTime.Now.Day;
+    //        }
+    //    }
+
+    //    public Animal(string gender)
+    //    {
+    //        Gender = gender;
+    //    }
+    //}
+
+
+    //internal class Dog : Animal
+    //{
+    //    public string Name { get; set; }
+
+
+
+    //    public Dog(string name,string gender)
+    //    {
+    //        Name = name;
+    //        Gender= gender;
+            
+    //    }
+    //}
+//}
